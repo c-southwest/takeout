a84c40846f10df38c11e0346232f8da71db2478a
c-southwest <1367248805@qq.com>
2024-03-18 23:50:29 +0100

    implement "dish pagination query"

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,20 +2,19 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,9 +28,17 @@ public class DishController {
 
     @PostMapping
     @ApiOperation("新增菜品save")
-    public Result save(@RequestBody DishDTO dishDTO){
+    public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavors(dishDTO);
         return Result.success();
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询page")
+    public Result<PageResult> page(DishPageQueryDTO pageQueryDTO) {
+        log.info("菜品分页查询：{}", pageQueryDTO);
+        PageResult pageResult = dishService.page(pageQueryDTO);
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,13 +1,12 @@
 package com.sky.service;
 
-import com.sky.dto.DishDTO;
-import com.sky.dto.EmployeeDTO;
-import com.sky.dto.EmployeeLoginDTO;
-import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.dto.*;
 import com.sky.entity.Employee;
 import com.sky.result.PageResult;
 
 public interface DishService {
 
     void saveWithFlavors(DishDTO dishDTO);
+
+    PageResult page(DishPageQueryDTO pageQueryDTO);
 }
